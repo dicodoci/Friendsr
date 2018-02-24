bcb4a351f365d1d156994ceaa8aff2c974800e5a
Dico <dico.vanleeuwen@student.uva.nl>
2018-02-24 17:39:01 +0100

    Added rating bar; Added comments

--- app/src/main/java/app/listview/pedor/com/friendsr/MainActivity.java
+++ app/src/main/java/app/listview/pedor/com/friendsr/MainActivity.java
@@ -23,13 +23,13 @@ public class MainActivity extends AppCompatActivity {
                           "Melisandre", "Sansa", "Tyrion"};
 
         // Create a bios for every person
-        String[] bios = {"hey ik ben dat kleine meisje dat helemaal doordraait",
+        String[] bios = {"Ik ben dat kleine meisje dat helemaal doordraait",
                 "Ben al in een relatie met mn broer",
                 "Ben al in een relatie met mn draken ofzo",
                 "Vroeger gebruikte ik mn hand, maar moet nu wat anders zoeken",
                 "Weet ik veel", "Ik ben verdikkeme nog steeds single",
                 "Ik probeer macht te krijgen door rare te glimlachen",
-                "ik hoor in een gesticht", "Ik ben jong en ik wil wat",
+                "Ik hoor in een gesticht", "Ik ben jong en ik wil wat",
                 "Ik ben eigenlijk helemaal niet zo heel erg lang"};
 
         // Create friends in loop based on the names
@@ -39,17 +39,21 @@ public class MainActivity extends AppCompatActivity {
             friends.add(new Friend(name, bio, getResources().getIdentifier(name.toLowerCase(),
                     "drawable", getPackageName())));
         }
+
+        // Adapter that fills the screen with all profiles
         FriendsAdapter adapter = new FriendsAdapter(this, R.layout.grid_item, friends);
         GridView grid = findViewById(R.id.grid);
         grid.setAdapter(adapter);
-        grid.setOnItemClickListener(new GridItemClickListener());
 
+        // Set on click listener for the grid
+        grid.setOnItemClickListener(new GridItemClickListener());
     }
+
+    // Class that enables going to individual profiles
     private class GridItemClickListener implements AdapterView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
             Friend clickedFriend = (Friend) adapterView.getItemAtPosition(i);
-            System.out.println(clickedFriend.getName());
             Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
             intent.putExtra("clicked_friend", clickedFriend);
             startActivity(intent);
--- app/src/main/java/app/listview/pedor/com/friendsr/ProfileActivity.java
+++ app/src/main/java/app/listview/pedor/com/friendsr/ProfileActivity.java
@@ -1,20 +1,28 @@
 package app.listview.pedor.com.friendsr;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    SharedPreferences.Editor editor;
+    Friend retrievedFriend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        Intent intent = getIntent();
-        Friend retrievedFriend = (Friend) intent.getSerializableExtra("clicked_friend");
 
+        // Get friend clicked
+        Intent intent = getIntent();
+        retrievedFriend = (Friend) intent.getSerializableExtra("clicked_friend");
+
+        // Set all views based on selected friend
         ImageView imageView = findViewById(R.id.picture);
         imageView.setImageResource(retrievedFriend.getDrawableId());
         TextView textViewName = findViewById(R.id.name);
@@ -22,5 +30,24 @@ public class ProfileActivity extends AppCompatActivity {
         TextView textViewBio = findViewById(R.id.bio);
         textViewBio.setText(retrievedFriend.getBio());
 
+        // Set rating back to old rating with as default value 0 stars
+        SharedPreferences prefs = getSharedPreferences("settings", MODE_PRIVATE);
+        editor = prefs.edit();
+        float rating = prefs.getFloat(retrievedFriend.getName(), (float) 0.0);
+        RatingBar ratingBar = findViewById(R.id.ratingBar);
+        ratingBar.setRating(rating);
+
+        // Set change listener for the rating bar
+        ratingBar.setOnRatingBarChangeListener(new RatingBarChangeListener());
+
+    }
+    private class RatingBarChangeListener implements RatingBar.OnRatingBarChangeListener {
+        @Override
+        public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+            System.out.println("pls no fuck");
+            editor.putFloat(retrievedFriend.getName(),v);
+            editor.apply();
+            retrievedFriend.setRating(v);
+        }
     }
 }
